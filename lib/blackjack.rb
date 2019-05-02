59c36cf114a34346222ecd7725d0556635163dde
Bryna Dunnells <badunnells@gmail.com>
2019-05-02 19:44:13 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,10 +39,11 @@ def hit?(card_total)
 
     if i == 'h'
      card_total += deal_card
-      display_card_total(card_total)
+  card_total(card_total)
       
     elsif i == 's'
-     display_card_total(card_total)
+      card_total
+      
     end
 
 end
